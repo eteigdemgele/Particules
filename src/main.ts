ab38d3ae26c1c680eae6204ddd5ec52ed57d1f8b
eteib dembele <dembeleeteib@gmail.com>
2023-09-21 10:30:22 +0200

    init

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,24 @@ const height = (canvas.height = window.innerHeight);
 ctx.fillStyle = "#000000";
 ctx.fillRect(0, 0, width, height);
 
-ctx.fillStyle = "#ffffff";
-ctx.arc(width / 2, height / 2, 10, 0, Math.PI * 2);
+ctx.fillStyle = "#ff0000";
+
+ctx.beginPath();
+ctx.arc(width-50 , height-50 ,20 , 0, Math.PI * 2);
 ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(width -50, 50 ,20 , 0, Math.PI * 2);
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(50 , 50 ,20 , 0, Math.PI * 2);
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(50 , height-50 ,20 , 0, Math.PI * 2);
+ctx.fill();
+ctx.closePath();
